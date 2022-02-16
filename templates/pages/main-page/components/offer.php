5609d3e7867c1f5f23d5d0d352ba803c921c0596
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-16 18:15:34 +0300

    offer created

--- templates/pages/main-page/components/offer.php
+++ templates/pages/main-page/components/offer.php
@@ -1,7 +1,7 @@
 <div class="hero-screen-offer">
 	<div class="container">
 		<div class="row">
-			<div class="col-xl-5">
+			<div class="col-xl-5 d-flex align-items-center">
                 <!--TODO ACF-->
                 <div class="offer">
                     <h1 class="offer-title">
@@ -11,17 +11,17 @@
                         Настройка за 7 минут. Минимум точек смазки b удобное расположение точек обслуживания. Низкая стоимость владения техникой.
                     </p>
                     <!--                TODO link to offer-->
-                    <a class="offer-link" href="#">
+                    <a class="btn-lg offer-link" href="#">
                         Заказать такой же
                     </a>
                 </div>
             </div>
-			<div class="col-xl-7">
+			<div class="col-xl-7 d-flex justify-content-xl-end justify-content-xll-end align-items-center">
                 <div class="slider-img">
 <!--                    TODO alt, src-->
-                    <img alt="slide" src="#">
+                    <img alt="slide" src="<?php echo get_template_directory_uri() . '/assets/dist/img/pages/main-page/hero-screen/slider.png' ?>">
+                    <div class="slider-bg"></div>
                 </div>
-                <div class="slider-bg"></div>
             </div>
 		</div>
 	</div>
