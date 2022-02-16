3dc93b1d9f7f190a47492d3752c3201b0104bd1a
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-16 17:08:11 +0300

    created offer template

--- templates/pages/main-page/components/offer.php
+++ templates/pages/main-page/components/offer.php
@@ -1,1 +1,28 @@
-
+<div class="hero-screen-offer">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-5">
+                <!--TODO ACF-->
+                <div class="offer">
+                    <h1 class="offer-title">
+                        Тормет – современный высокопроизводительный культиватор
+                    </h1>
+                    <p class="offer-description">
+                        Настройка за 7 минут. Минимум точек смазки b удобное расположение точек обслуживания. Низкая стоимость владения техникой.
+                    </p>
+                    <!--                TODO link to offer-->
+                    <a class="offer-link" href="#">
+                        Заказать такой же
+                    </a>
+                </div>
+            </div>
+			<div class="col-xl-7">
+                <div class="slider-img">
+<!--                    TODO alt, src-->
+                    <img alt="slide" src="#">
+                </div>
+                <div class="slider-bg"></div>
+            </div>
+		</div>
+	</div>
+</div>
