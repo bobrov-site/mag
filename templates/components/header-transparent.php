fa35a925d7311a5920557238bce95318796ea5f9
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-16 14:07:18 +0300

    add TODO

--- templates/components/header-transparent.php
+++ templates/components/header-transparent.php
@@ -9,6 +9,7 @@
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <!--            //TODO Custom Menu -->
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li class="nav-item">
                                     <a class="nav-link active" aria-current="page" href="#">Каталог</a>
