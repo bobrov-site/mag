d3c0e52a5975183e7d7c30123743c64587a14f46
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-16 16:53:47 +0300

    created hero-screen.php and offer.php temples

--- index.php
+++ index.php
@@ -4,10 +4,7 @@ get_header();
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
-<!--            //TODO ACF -->
-            <section class="hero-screen" style="background: linear-gradient(0deg,rgba(0,0,0,.4),rgba(0,0,0,.4)) ,url(<?php echo get_template_directory_uri() . '/assets/dist/img/pages/main-page/hero-screen/hero-bg.jpg'?>) center center; background-size: cover;">
-	            <?php get_template_part('templates/components/header-transparent'); ?>
-            </section>
+            <?php get_template_part('templates/pages/main-page/components/hero-screen'); ?>
         </main><!-- #main -->
     </div><!-- #primary -->
 
