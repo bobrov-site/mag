cb75bdd2eb330786bdba79b57a65aab4d72c5580
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-16 14:06:37 +0300

    nav created

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ get_header();
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
-            <section class="hero-screen" style="background: url(<?php echo get_template_directory_uri() . '/assets/dist/img/pages/main-page/hero-screen/slider.png' ?>)">
+<!--            //TODO ACF -->
+            <section class="hero-screen" style="background: linear-gradient(0deg,rgba(0,0,0,.4),rgba(0,0,0,.4)) ,url(<?php echo get_template_directory_uri() . '/assets/dist/img/pages/main-page/hero-screen/hero-bg.jpg'?>) center center; background-size: cover;">
 	            <?php get_template_part('templates/components/header-transparent'); ?>
             </section>
         </main><!-- #main -->
