937743d41b760d3ef1110eaed35aab9e644126d0
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-17 19:41:53 +0300

    our products completed

--- templates/components/our-products-action.php
+++ templates/components/our-products-action.php
@@ -1,1 +1,31 @@
-<?php
+<section class="our-products-action">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl">
+				<img class="our-products-action-img" src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/our-products/product.png' ?>" alt="our-products-action">
+			</div>
+			<div class="col-xl d-flex align-items-center">
+				<div class="our-products-action-offer">
+					<h2 class="title-section dark-section">
+						Наша собственная продукция
+					</h2>
+					<ol class="list-offer list-group-numbered">
+						<li class="offer-item">
+							Современное оборудование и технологии
+						</li>
+						<li class="offer-item">
+							Применение методов Лин на предприятии
+						</li>
+						<li class="offer-item">
+							Опытные и высоко квалифицированные кадры
+						</li>
+					</ol>
+					<a class="btn btn-secondary btn-lg">
+						Посмотреть продукцию
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+    <div class="gradient-section"></div>
+</section>
