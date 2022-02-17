8b3c6cda4734f6decfbd3c1aeddfa67b7be6528b
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-17 20:08:35 +0300

    feedbacks created

--- templates/components/feedbacks.php
+++ templates/components/feedbacks.php
@@ -1,1 +1,62 @@
-<?php
+<section class="feedbacks">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="brick"></div>
+				<h2 class="title-section">
+					Отзывы наших покупателей
+				</h2>
+			</div>
+		</div>
+		<div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+			<div class="col">
+				<div class="card h-100">
+					<div class="card-body">
+<!--                        TODO ACF-->
+                        <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/feedbacks/avatar.png' ?>" class="card-img-avatar" alt="...">
+						<h3 class="card-title">
+                            Светлана Быкова
+						</h3>
+						<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					</div>
+				</div>
+			</div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <!--                        TODO ACF-->
+                        <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/feedbacks/avatar.png' ?>" class="card-img-avatar" alt="...">
+                        <h3 class="card-title">
+                            Светлана Быкова
+                        </h3>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <!--                        TODO ACF-->
+                        <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/feedbacks/avatar.png' ?>" class="card-img-avatar" alt="...">
+                        <h3 class="card-title">
+                            Светлана Быкова
+                        </h3>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <!--                        TODO ACF-->
+                        <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/feedbacks/avatar.png' ?>" class="card-img-avatar" alt="...">
+                        <h3 class="card-title">
+                            Светлана Быкова
+                        </h3>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+		</div>
+	</div>
+</section>
