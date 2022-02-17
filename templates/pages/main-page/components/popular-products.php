cef883f25708ac00a463304df954a2666c883e67
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-17 14:36:22 +0300

    created cards

--- templates/pages/main-page/components/popular-products.php
+++ templates/pages/main-page/components/popular-products.php
@@ -8,5 +8,20 @@
                 </h2>
             </div>
         </div>
+<!--        TODO to Cards ACF-->
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+            <div class="col">
+                <div class="card">
+                    <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/popular-products/Example-card.png' ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <a class="btn btn-primary">
+                            Подробнее
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
