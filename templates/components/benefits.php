5acb55d740ac1d8c95c762f052c6c86714eba961
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-17 19:15:51 +0300

    benefits completed

--- templates/components/benefits.php
+++ templates/components/benefits.php
@@ -1,1 +1,75 @@
-<?php
+<section class="benefits">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-6">
+				<div class="brick"></div>
+				<h2 class="title-section">Преимущества покупки продукции у ООО “МАГ”</h2>
+			</div>
+		</div>
+        <div class="row">
+            <div class="col-xl-6">
+                <div class="benefits-list">
+                    <div class="row">
+                        <div class="col-xl-6">
+                            <div class="benefit">
+                                <img class="benefit-icon" alt="" src="<?php echo get_template_directory_uri() . '/assets/dist/svg/components/benefits/headphones.svg' ?>">
+                                <h3 class="benefit-title">
+                                    Тех. поддержка 24/7
+                                </h3>
+                                <p class="benefit-desc">
+                                    Большая команда инженеров и молодых
+                                    специалистов ответят на все ваши вопросы
+                                    по технике.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-xl-6">
+                            <div class="benefit">
+                                <img class="benefit-icon" alt="" src="<?php echo get_template_directory_uri() . '/assets/dist/svg/components/benefits/handshake.svg' ?>">
+                                <h3 class="benefit-title">
+                                    Всегда идем навстречу клиенту
+                                </h3>
+                                <p class="benefit-desc">
+                                    Наша компания подстраивается по индивидуальные потребности фермеров за получением идеального орудия для хозяйства и развития бизнеса клиента.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xl-6">
+                            <div class="benefit">
+                                <img class="benefit-icon" alt="" src="<?php echo get_template_directory_uri() . '/assets/dist/svg/components/benefits/checkbox.svg' ?>">
+                                <h3 class="benefit-title">
+                                    Инновационные решения
+                                </h3>
+                                <p class="benefit-desc">
+                                    Произведенные нами орудия отвечают всем требованиям современного фермера и становятся новым почвообрабатывающим трендом. 
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-xl-6">
+                            <div class="benefit">
+                                <img class="benefit-icon" alt="" src="<?php echo get_template_directory_uri() . '/assets/dist/svg/components/benefits/gear.svg' ?>">
+                                <h3 class="benefit-title">
+                                    Собственное производство в городе Ставрополь
+                                </h3>
+                                <p class="benefit-desc">
+                                    Производственная площадь в г. Ставрополь и два прогрессивных орудия собственного производства: универсальный культиватор Termet и борона-мотыга ротационная Rubble.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="btn btn-primary btn-lg">
+                    Перейти в каталог
+                </a>
+            </div>
+            <div class="col-xl-6 d-flex justify-content-end">
+                <div class="benefits-img">
+<!--                    //TODO ACF-->
+                    <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/benefits/woman.png' ?>" alt="benefit">
+                </div>
+            </div>
+        </div>
+	</div>
+</section>
