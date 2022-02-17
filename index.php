340327a2570178627deb33fe828a6b7f80f67dba
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-17 13:25:40 +0300

    created templates for main page and site

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ get_header();
         <main id="main" class="site-main">
             <?php get_template_part('templates/pages/main-page/components/hero-screen'); ?>
             <?php get_template_part('templates/pages/main-page/components/popular-products'); ?>
+            <?php get_template_part('templates/pages/main-page/components/socials-action'); ?>
+            <?php get_template_part('templates/components/benefits'); ?>
+            <?php get_template_part('templates/components/our-products-action'); ?>
+            <?php get_template_part('templates/components/feedbacks'); ?>
+            <?php get_template_part('templates/components/our-partners-action'); ?>
+            <?php get_template_part('templates/pages/main-page/components/leasing-action'); ?>
+            <?php get_template_part('templates/components/news'); ?>
+            <?php get_template_part('templates/components/company-action'); ?>
+            <?php get_template_part('templates/components/contact-form'); ?>
         </main><!-- #main -->
     </div><!-- #primary -->
 
