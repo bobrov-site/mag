f9ec94e7f9f2a9cc765a47b079b2eab99a96754a
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-17 13:07:23 +0300

    added popular products template

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ get_header();
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
             <?php get_template_part('templates/pages/main-page/components/hero-screen'); ?>
+            <?php get_template_part('templates/pages/main-page/components/popular-products'); ?>
         </main><!-- #main -->
     </div><!-- #primary -->
 
