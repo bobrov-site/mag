d9edd109a8b6923da136e54b74ea3678595fec68
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-17 13:41:34 +0300

    created brick and title section

--- templates/pages/main-page/components/popular-products.php
+++ templates/pages/main-page/components/popular-products.php
@@ -2,8 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
-                <h2 class="title-section white-bg">
-
+                <div class="brick"></div>
+                <h2 class="title-section">
+                    Популярная продукция
                 </h2>
             </div>
         </div>
