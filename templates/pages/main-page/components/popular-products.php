83d1bc8f67a9416e41c3f2e0abd1e05a2b98c35a
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-17 13:28:58 +0300

    create _popular-products.scss and added title-section

--- templates/pages/main-page/components/popular-products.php
+++ templates/pages/main-page/components/popular-products.php
@@ -1,3 +1,11 @@
 <section id="popular-products" class="popular-products">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <h2 class="title-section white-bg">
 
+                </h2>
+            </div>
+        </div>
+    </div>
 </section>
