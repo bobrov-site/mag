ed1c76ab76d5d51f0ac496c85b630725d248b805
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-18 19:22:31 +0300

    contact-form.php created

--- templates/components/contact-form.php
+++ templates/components/contact-form.php
@@ -1,1 +1,38 @@
-<?php
+<section class="contact-form">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="brick"></div>
+				<h2 class="title-section">
+					Остались еще вопросы?
+				</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xl-5">
+				<div class="form">
+					<h3 class="form-title">
+						Введите ваше имя, контактный номер телефона и мы свяжемся с Вами в течение 15 минут
+					</h3>
+                    <div>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Ваше имя">
+                    </div>
+                    <div>
+                        <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="Ваше номер телефона">
+                    </div>
+                    <a class="btn btn-primary">
+                        Отправить заявку
+                    </a>
+                    <p class="privacy">
+                        Нажимая на кнопку, вы даете согласие на обработку персональных данных и соглашаетесь с <a href="#" class="privacy-link">
+                            политикой конфиденциальности
+                        </a>
+                    </p>
+				</div>
+			</div>
+			<div class="col-xl-7 d-flex justify-content-end">
+				<iframe class="contact-form-map" src="https://yandex.ru/map-widget/v1/?um=constructor%3A0d65e56db49bc5d067672b3a2ad9eb8ba0b87191e8f449a1fd5ac00ec727b0ba&amp;source=constructor" width="546" height="100%" frameborder="0"></iframe>
+			</div>
+		</div>
+	</div>
+</section>
