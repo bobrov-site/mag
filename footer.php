fa4ed8a3c760c512d62ecb902d6b43418c5e6336
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-18 21:12:04 +0300

    footer created

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 	<footer id="colophon" class="site-footer">
 		<div class="container">
             <div class="row">
-                <div class="col-xl">
+                <div class="col-xl-3">
                     <img src="<?php echo get_template_directory_uri() . '/assets/src/img/components/footer/logo.png'?>">
                 </div>
                 <div class="col-xl-2">
@@ -26,8 +26,81 @@
                         </li>
                     </ul>
                 </div>
+                <div class="col-xl-3">
+                    <div class="footer-address">
+                        <div class="address-info">
+                            <h4 class="address-info-title">
+                                Главный офис:
+                            </h4>
+                            <p class="address-info-text">
+                                г. Михайловск 356245 <br>
+                                ул. Вокзальная 22 <br>
+                                2 этаж, каб 14 <br>
+                            </p>
+                        </div>
+                        <div class="address-info">
+                            <h4 class="address-info-title">
+                                Наше производство:
+                            </h4>
+                            <p class="address-info-text">
+                                г. Ставрополь 355044 <br>
+                                пр. Кулакова, 18
+                            </p>
+                        </div>
+                        <div class="address-info">
+                            <h4 class="address-info-title">
+                                График работы:
+                            </h4>
+                            <p class="address-info-text">
+                                ПН-ПТ с 9:00 до 17:00
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl d-flex justify-content-end">
+                    <a href="#" class="footer-number">
+                        <!--                        TODO number-->
+                        +7 (962) 444-40-53
+                    </a>
+                    <div class="callback-and-social">
+                        <div class="callback">
+                            <!--                        TODO callback-->
+                            <a class="btn btn-primary" href="#">
+                                <div class="callback-icon"></div>
+                                <span class="callback-text">
+                                    Заказать звонок
+                                </span>
+                            </a>
+                        </div>
+                        <div class="socials d-flex justify-content-end">
+<!--                            TODO Get links-->
+                            <a class="social-link" href="#">
+                                <img class="social-icon" src="<?php echo get_template_directory_uri() . '/assets/dist/svg/components/footer/instagram.svg'?>" alt="instagram">
+                            </a>
+                            <a class="social-link" href="#">
+                                <img class="social-icon" src="<?php echo get_template_directory_uri() . '/assets/dist/svg/components/footer/youtube.svg'?>" alt="youtube">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
                 <div class="col-xl">
-
+                    <span class="copyright">
+                        © 2022 ООО “МАГ”
+                    </span>
+                </div>
+                <div class="col-xl">
+                    <a class="developer" href="taplink.cc/bobrov_site">
+                        Сайт разработал Бобров Даниил
+                    </a>
+                </div>
+                <div class="col-xl">
+<!--                    TODO Add page-->
+                    <a class="privacy-policy" href="#">
+                        Политика конфиденциальности
+                    </a>
                 </div>
             </div>
         </div>
