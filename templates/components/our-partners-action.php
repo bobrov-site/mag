b4ed8a8534d191c4bc3195bf62370584f0c7e682
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-18 11:55:34 +0300

    our-partners-action.php created

--- templates/components/our-partners-action.php
+++ templates/components/our-partners-action.php
@@ -1,1 +1,30 @@
-<?php
+<section class="our-partners-action">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="brick"></div>
+				<h2 class="title-section">Наши партнеры</h2>
+			</div>
+		</div>
+        <div class="row">
+            <div class="col-xl-6">
+                <div class="partners-offer">
+<!--                    TODO ACF-->
+                    <img class="partners-offer-img" src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/our-partners/hands.png' ?>" alt="partner">
+                    <h3 class="partners-offer-title">
+                        Хотите стать нашим партнером?
+                    </h3>
+                    <p class="partners-offer-desc">
+                        Оставьте заявку на нашем сайте и мы рассмотрем ваше предложение.
+                    </p>
+                    <a class="btn btn-primary">
+                        Стать партнером
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-6">
+                <div class="partners-slider"></div>
+            </div>
+        </div>
+	</div>
+</section>
