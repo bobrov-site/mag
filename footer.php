464d958814e06d437b2ca9e3f1c95bce7eff9754
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-18 20:04:37 +0300

    created footer

--- footer.php
+++ footer.php
@@ -2,7 +2,35 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		
+		<div class="container">
+            <div class="row">
+                <div class="col-xl">
+                    <img src="<?php echo get_template_directory_uri() . '/assets/src/img/components/footer/logo.png'?>">
+                </div>
+                <div class="col-xl-2">
+                    <ul class="nav nav-pills ">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Каталог</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Производство</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Дилеры</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">О компании</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-xl">
+
+                </div>
+            </div>
+        </div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
