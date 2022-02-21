40c63f675ff49ac183c637971951549de679eb4f
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-21 16:04:26 +0300

    created about section

--- page-o-nas.php
+++ page-o-nas.php
@@ -5,10 +5,9 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<?php get_template_part('templates/components/hero-screen') ?>
-
+            <?php get_template_part('templates/pages/company/components/about') ?>
 			<?php get_template_part('templates/components/benefits') ?>
 			<?php get_template_part('templates/components/social-action') ?>
-<!--			sertification -->
 			<?php get_template_part('/templates/components/our-partners-action') ?>
 			<?php get_template_part('/templates/components/feedbacks') ?>
 			<?php get_template_part('templates/components/our-products-action') ?>
