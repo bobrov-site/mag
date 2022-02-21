a403475872215eed38aa9f1f4f0d41a65679ad65
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-21 10:15:45 +0300

    created components for hero-screen and page-kontakty.php

---
+++ page-kontakty.php
@@ -0,0 +1,13 @@
+<?php
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php get_template_part('templates/components/hero-screen') ?>
+			<?php get_template_part('templates/components/contact-form'); ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
---
+++ templates/components/breadcrumbs.php
@@ -0,0 +1,14 @@
+<div class="container">
+	<div class="row">
+		<div class="col-xl-12">
+			<div class="breadcrumb"></div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xl-12">
+			<h1 class="title-page">
+                Контакты
+			</h1>
+		</div>
+	</div>
+</div>
