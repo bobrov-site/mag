4c2548a952f20e9c229977859341ef49ff3e3cd7
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-21 13:31:07 +0300

    created requisites collapse

--- assets/src/js/_bootstrapPlugins.js
+++ assets/src/js/_bootstrapPlugins.js
@@ -1,15 +1,32 @@
-import { Dropdown } from 'bootstrap';
+import { Dropdown, Collapse } from 'bootstrap';
 
 require('bootstrap');
 
 class BootstrapPlugins {
 	constructor() {
 		this.dropdownInit();
+		this.collapseInit();
+		this.collapseRequisites();
 	}
 
 	// eslint-disable-next-line class-methods-use-this
 	dropdownInit() {
 		return Dropdown;
+	}
+
+	// eslint-disable-next-line class-methods-use-this
+	collapseInit() {
+		return Collapse;
+	}
+
+	// eslint-disable-next-line class-methods-use-this
+	collapseRequisites() {
+		const collapse = document.getElementById('collapseRequisites');
+		// eslint-disable-next-line no-undef
+		const bscollapse = new Collapse(collapse, {
+			toggle: false,
+		});
+		bscollapse.show();
 	}
 }
 
