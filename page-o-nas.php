29966996fac8aa0abe581e29212338544eda7ea3
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-21 14:12:02 +0300

    created page about

---
+++ page-o-nas.php
@@ -0,0 +1,20 @@
+<?php
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php get_template_part('templates/components/hero-screen') ?>
+
+			<?php get_template_part('templates/components/benefits') ?>
+			<?php get_template_part('templates/components/social-action') ?>
+<!--			sertification -->
+			<?php get_template_part('/templates/components/our-partners-action') ?>
+			<?php get_template_part('/templates/components/feedbacks') ?>
+			<?php get_template_part('templates/components/our-products-action') ?>
+			<?php get_template_part('templates/components/contact-form'); ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
