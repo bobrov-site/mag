69a32748f937307b42db12d1dfbcef3d9a71ae6d
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-21 10:40:29 +0300

    created requisites.php

--- page-kontakty.php
+++ page-kontakty.php
@@ -5,6 +5,7 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<?php get_template_part('templates/components/hero-screen') ?>
+            <?php get_template_part('templates/pages/contacts/components/requisites'); ?>
 			<?php get_template_part('templates/components/contact-form'); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
