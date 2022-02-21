7982ef9c6acf64687f92d728e2670673d980a1cb
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-21 16:10:40 +0300

    changed paths

--- page-o-nas.php
+++ page-o-nas.php
@@ -7,9 +7,9 @@ get_header();
 			<?php get_template_part('templates/components/hero-screen') ?>
             <?php get_template_part('templates/pages/company/components/about') ?>
 			<?php get_template_part('templates/components/benefits') ?>
-			<?php get_template_part('templates/components/social-action') ?>
-			<?php get_template_part('/templates/components/our-partners-action') ?>
-			<?php get_template_part('/templates/components/feedbacks') ?>
+			<?php get_template_part('templates/components/socials-action') ?>
+			<?php get_template_part('templates/components/our-partners-action') ?>
+			<?php get_template_part('templates/components/feedbacks') ?>
 			<?php get_template_part('templates/components/our-products-action') ?>
 			<?php get_template_part('templates/components/contact-form'); ?>
 		</main><!-- #main -->
