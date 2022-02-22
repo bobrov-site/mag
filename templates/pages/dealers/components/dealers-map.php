7d9dda3af3036c12ab4bb72bd5701d9756192823
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-22 12:19:52 +0300

    add map inframe

--- templates/pages/dealers/components/dealers-map.php
+++ templates/pages/dealers/components/dealers-map.php
@@ -8,10 +8,8 @@
 				</h2>
 			</div>
 		</div>
-		<div class="row">
-			<div class="dealers-map-block">
-
-			</div>
-		</div>
 	</div>
+    <div class="dealers-map-block">
+        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A44d28b08d4101e00ae807f64688e231b8dae23cf36d3a2cf7d12267c7e000af3&amp;source=constructor" width="100%" height="499" frameborder="0"></iframe>
+    </div>
 </section>
