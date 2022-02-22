a2e4c3f86aa4b47d7ee4d972c64b709d98fb7a61
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-22 11:30:08 +0300

    created dealers.php

--- templates/pages/dealers/components/dealers.php
+++ templates/pages/dealers/components/dealers.php
@@ -1,1 +1,177 @@
-<?php
+<section class="dealers">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="brick"></div>
+				<h2 class="title-section">
+					Наши официальные дилеры
+				</h2>
+			</div>
+		</div>
+		<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+<!--            TODO ACF-->
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            ООО «Агро –Склад 63»
+                        </h4>
+                        <p class="card-text">
+                            г. Самара, Самарская об-ть
+                        </p>
+                        <div class="card-number">
+                            <a class="card-number-link" href="tel:<?php echo '' ?>">
+                                7 (962) 444-40-53
+                            </a>
+                        </div>
+                        <a class="btn btn-primary" href="<?php echo '' ?>">Перейти на сайт дилера</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            ООО «Агро –Склад 63»
+                        </h4>
+                        <p class="card-text">
+                            г. Самара, Самарская об-ть
+                        </p>
+                        <div class="card-number">
+                            <a class="card-number-link" href="tel:<?php echo '' ?>">
+                                7 (962) 444-40-53
+                            </a>
+                        </div>
+                        <a class="btn btn-primary" href="<?php echo '' ?>">Перейти на сайт дилера</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            ООО «Агро –Склад 63»
+                        </h4>
+                        <p class="card-text">
+                            г. Самара, Самарская об-ть
+                        </p>
+                        <div class="card-number">
+                            <a class="card-number-link" href="tel:<?php echo '' ?>">
+                                7 (962) 444-40-53
+                            </a>
+                        </div>
+                        <a class="btn btn-primary" href="<?php echo '' ?>">Перейти на сайт дилера</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            ООО «Агро –Склад 63»
+                        </h4>
+                        <p class="card-text">
+                            г. Самара, Самарская об-ть
+                        </p>
+                        <div class="card-number">
+                            <a class="card-number-link" href="tel:<?php echo '' ?>">
+                                7 (962) 444-40-53
+                            </a>
+                        </div>
+                        <a class="btn btn-primary" href="<?php echo '' ?>">Перейти на сайт дилера</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            ООО «Агро –Склад 63»
+                        </h4>
+                        <p class="card-text">
+                            г. Самара, Самарская об-ть
+                        </p>
+                        <div class="card-number">
+                            <a class="card-number-link" href="tel:<?php echo '' ?>">
+                                7 (962) 444-40-53
+                            </a>
+                        </div>
+                        <a class="btn btn-primary" href="<?php echo '' ?>">Перейти на сайт дилера</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            ООО «Агро –Склад 63»
+                        </h4>
+                        <p class="card-text">
+                            г. Самара, Самарская об-ть
+                        </p>
+                        <div class="card-number">
+                            <a class="card-number-link" href="tel:<?php echo '' ?>">
+                                7 (962) 444-40-53
+                            </a>
+                        </div>
+                        <a class="btn btn-primary" href="<?php echo '' ?>">Перейти на сайт дилера</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            ООО «Агро –Склад 63»
+                        </h4>
+                        <p class="card-text">
+                            г. Самара, Самарская об-ть
+                        </p>
+                        <div class="card-number">
+                            <a class="card-number-link" href="tel:<?php echo '' ?>">
+                                7 (962) 444-40-53
+                            </a>
+                        </div>
+                        <a class="btn btn-primary" href="<?php echo '' ?>">Перейти на сайт дилера</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            ООО «Агро –Склад 63»
+                        </h4>
+                        <p class="card-text">
+                            г. Самара, Самарская об-ть
+                        </p>
+                        <div class="card-number">
+                            <a class="card-number-link" href="tel:<?php echo '' ?>">
+                                7 (962) 444-40-53
+                            </a>
+                        </div>
+                        <a class="btn btn-primary" href="<?php echo '' ?>">Перейти на сайт дилера</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            ООО «Агро –Склад 63»
+                        </h4>
+                        <p class="card-text">
+                            г. Самара, Самарская об-ть
+                        </p>
+                        <div class="card-number">
+                            <a class="card-number-link" href="tel:<?php echo '' ?>">
+                                7 (962) 444-40-53
+                            </a>
+                        </div>
+                        <a class="btn btn-primary" href="<?php echo '' ?>">Перейти на сайт дилера</a>
+                    </div>
+                </div>
+            </div>
+		</div>
+	</div>
+</section>
