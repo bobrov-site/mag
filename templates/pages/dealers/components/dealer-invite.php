5e0d171e855e91495c2af341ff4e724927cb2b3f
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-22 10:57:29 +0300

    created dealer-invite.php and _dealer-invite.scss

--- templates/pages/dealers/components/dealer-invite.php
+++ templates/pages/dealers/components/dealer-invite.php
@@ -1,1 +1,20 @@
-
+<section class="dealer-invite">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-5">
+				<h2 class="title-section dark-section">
+					ООО “Монолит Агро Групп” приглашает вас стать официальным дилером
+				</h2>
+				<p class="title-desc"></p>
+				<a class="btn btn-secondary">
+					Стать дилером
+				</a>
+			</div>
+			<div class="col-xl-7">
+<!--                TODO ACF-->
+				<img src="<?php echo get_template_directory_uri() . '/assets/src/img/pages/dealers/dealer.png' ?>">
+			</div>
+		</div>
+	</div>
+	<img class="gradient-section" alt="gradient-products" src="<?php echo get_template_directory_uri() . '/assets/src/svg/pages/dealers/components/dealer-invite/gradient.svg'?>">
+</section>
