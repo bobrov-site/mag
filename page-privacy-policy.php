8ce94fa4d06510b66d14c27ea99a8a5712a3e4aa
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-22 15:06:46 +0300

    policy created

---
+++ page-privacy-policy.php
@@ -0,0 +1,14 @@
+<?php
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php get_template_part('templates/components/hero-screen') ?>
+			<?php get_template_part('templates/pages/policy/policy') ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
+
---
+++ templates/pages/policy/policy.php
@@ -0,0 +1,11 @@
+<section class="policy-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="policy-content">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
