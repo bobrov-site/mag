77771a926aa225d6cbe466fb69bd4809e9755622
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-22 11:11:07 +0300

    remove /

--- page-dilery.php
+++ page-dilery.php
@@ -8,7 +8,7 @@ get_header();
             <?php get_template_part('templates/pages/dealers/components/dealer-invite') ?>
 			<?php get_template_part('templates/pages/dealers/components/dealers') ?>
 			<?php get_template_part('templates/pages/dealers/components/dealers-map') ?>
-            \<?php get_template_part('templates/pages/dealers/components/dealers-blank') ?>
+            <?php get_template_part('templates/pages/dealers/components/dealers-blank') ?>
 			<?php get_template_part('templates/components/contact-form'); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
