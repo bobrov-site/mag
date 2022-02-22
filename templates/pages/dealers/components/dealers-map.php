157a021006b84f75be361ca3ccd020e55d2c654c
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-22 12:13:02 +0300

    created dealers-map.php

--- templates/pages/dealers/components/dealers-map.php
+++ templates/pages/dealers/components/dealers-map.php
@@ -1,1 +1,17 @@
-<?php
+<section class="dealers-map">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="brick"></div>
+				<h2 class="title-section">
+					Официальные дилеры на карте
+				</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="dealers-map-block">
+
+			</div>
+		</div>
+	</div>
+</section>
