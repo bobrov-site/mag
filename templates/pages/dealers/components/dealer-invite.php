a5c2fa4db06b89f1f8d9a1df2867128c3dcee3c8
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-22 11:10:58 +0300

    updated _dealer-invite.scss

--- templates/pages/dealers/components/dealer-invite.php
+++ templates/pages/dealers/components/dealer-invite.php
@@ -1,18 +1,22 @@
 <section class="dealer-invite">
 	<div class="container">
 		<div class="row">
-			<div class="col-xl-5">
-				<h2 class="title-section dark-section">
-					ООО “Монолит Агро Групп” приглашает вас стать официальным дилером
-				</h2>
-				<p class="desc-section"></p>
-				<a href="#dealers-blank" class="btn btn-secondary">
-					Стать дилером
-				</a>
+			<div class="col-xl-5 d-flex align-items-center">
+				<div class="dealer-invite-offer">
+                    <h2 class="title-section dark-section">
+                        ООО “Монолит Агро Групп” приглашает вас стать официальным дилером
+                    </h2>
+                    <p class="desc-section">
+                        Благодарим Вас за интерес, проявленный к дилерам компании ООО «Монолит Агро групп»
+                    </p>
+                    <a href="#dealers-blank" class="btn btn-secondary btn-lg">
+                        Стать дилером
+                    </a>
+                </div>
 			</div>
-			<div class="col-xl-7">
+			<div class="col-xl-7 d-flex justify-content-end">
 <!--                TODO ACF-->
-				<img src="<?php echo get_template_directory_uri() . '/assets/src/img/pages/dealers/dealer.png' ?>" alt="dealer">
+				<img class="dealer-invite-img" src="<?php echo get_template_directory_uri() . '/assets/src/img/pages/dealers/dealer.png' ?>" alt="dealer">
 			</div>
 		</div>
 	</div>
