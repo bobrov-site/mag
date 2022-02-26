05f124bf82861f66a47260a4c658437b2d599f17
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-26 09:36:20 +0300

    added php code to gallery-grid.php

--- templates/pages/gallery/gallery-grid.php
+++ templates/pages/gallery/gallery-grid.php
@@ -8,46 +8,34 @@ $arg_category = array(
 $categories = get_categories( $arg_category );
 ?>
 
-<?php
-if( $categories ){
-	foreach( $categories as $cat ){
-		$mypost = array(
-			'post_type' => 'gallery',
-			'tax_query' => array(
-				array(
-					'taxonomy' => 'years',
-					'field' => 'slug',
-					'terms' => $cat->slug
-				),
-			),
-		);
-		$loop = new WP_Query( $mypost ); ?>
-
-        <?php if ($loop->have_posts()) ?>
-            <h2><?php echo $cat->name; ?> </h2>
-		<?php while ( $loop->have_posts() ) : $loop->the_post();?>
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
-                </div>
-            </article>
-		<?php endwhile; ?>
-		<?php
-	}
-}
-?>
-
 <section class="gallery-grid">
+	<?php
+	if( $categories ){
+		foreach( $categories as $cat ){
+			$mypost = array(
+				'post_type' => 'gallery',
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'years',
+						'field' => 'slug',
+						'terms' => $cat->slug
+					),
+				),
+			);
+			$loop = new WP_Query( $mypost ); ?>
+	<?php if ($loop->have_posts()) ?>
     <div class="gallery-grid-year">
         <div class="container">
             <div class="row">
                 <div class="col-xl-12">
-                    <h2 class="title-section"><?php the_taxonomies(); ?></h2>
+                    <h2 class="title-section"><?php echo $cat->name; ?></h2>
                 </div>
             </div>
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
                 <div class="col">
                     <div class="card h-100">
+<!--                        TODO ALT-->
                         <img src="<?php echo get_the_post_thumbnail_url() ?>" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title">
@@ -60,7 +48,9 @@ if( $categories ){
                         </div>
                     </div>
                 </div>
+        <?php endwhile; ?>
             </div>
         </div>
     </div>
+	<?php } } ?>
 </section>
