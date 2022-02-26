5bec0966054eb751e5999b13dff633d04f9bae67
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-26 13:03:07 +0300

    created news-grid-archive.php

--- taxonomy-news_categories.php
+++ taxonomy-news_categories.php
@@ -5,7 +5,7 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<?php get_template_part('templates/components/hero-screen') ?>
-			<?php get_template_part('templates/pages/news/news-grid'); ?>
+			<?php get_template_part('templates/pages/news/news-grid-archive'); ?>
 			<?php get_template_part('templates/components/our-products-action') ?>
 			<?php get_template_part('templates/components/contact-form'); ?>
 		</main><!-- #main -->
