de5a6636395dfdd236d26a3226665bdb0ad0a9d8
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-26 12:05:20 +0300

    added templates

--- taxonomy-news_categories.php
+++ taxonomy-news_categories.php
@@ -1,3 +1,15 @@
 <?php
+get_header();
+?>
 
-echo 'hello';
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php get_template_part('templates/components/hero-screen') ?>
+			<?php get_template_part('templates/pages/news/news-grid'); ?>
+			<?php get_template_part('templates/components/our-products-action') ?>
+			<?php get_template_part('templates/components/contact-form'); ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
