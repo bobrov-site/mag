35470325175726314649b46a861b9209d84c9451
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-28 13:51:04 +0300

    added new dynamic menu

--- templates/components/header-transparent.php
+++ templates/components/header-transparent.php
@@ -7,32 +7,17 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!--            //TODO Custom Menu -->
-                <ul class="navbar-nav nav-pills me-auto">
-                    <li class="nav-item">
-                        <!--            //TODO Active item -->
-                        <a class="nav-link active" aria-current="page" href="#">Каталог</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Производство</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Новости</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Дилеры</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            О компании
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">О нас</a></li>
-                            <li><a class="dropdown-item" href="#">Контакты</a></li>
-                            <li><a class="dropdown-item" href="#">Галерея</a></li>
-                            <li><a class="dropdown-item" href="#">Лизинг</a></li>
-                        </ul>
-                    </li>
-                </ul>
+	            <?php
+	            wp_nav_menu(array(
+		            'theme_location' => 'main-menu',
+		            'container' => false,
+		            'menu_class' => '',
+		            'fallback_cb' => '__return_false',
+		            'items_wrap' => '<ul id="%1$s" class="navbar-nav nav-pills me-auto %2$s">%3$s</ul>',
+		            'depth' => 2,
+		            'walker' => new bootstrap_5_wp_nav_menu_walker()
+	            ));
+	            ?>
                 <!--            //TODO ACF Number -->
                 <a href="tel:<?php echo '+7 (962) 444-40-53' ?>" class="navbar-number">
                     +7 (962) 444-40-53
