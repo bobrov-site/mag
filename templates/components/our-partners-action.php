71a80304287add8cf460388e463b5309e30e0bf2
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-28 19:57:33 +0300

    created partners loop

--- templates/components/our-partners-action.php
+++ templates/components/our-partners-action.php
@@ -9,7 +9,6 @@
         <div class="row">
             <div class="col-xl-6">
                 <div class="partners-offer">
-<!--                    TODO ACF-->
                     <img class="partners-offer-img" src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/our-partners/hands.png' ?>" alt="partner">
                     <h3 class="partners-offer-title">
                         Хотите стать нашим партнером?
@@ -25,19 +24,36 @@
             <div class="col-xl-6">
                 <div class="partners-slider d-flex align-items-center justify-content-center border">
                     <div class="partners-swiper">
-                        <div class="swiper-wrapper">
+                        <div class="swiper-wrapper d-flex align-items-center">
+	                        <?php
+	                        $mypost = array(
+		                        'post_type' => 'partners',
+		                        'order' => 'ASC',
+		                        'tax_query' => array(
+			                        array(
+				                        'taxonomy' => 'partners_categories',
+				                        'field' => 'slug',
+				                        'terms' => 'partnery'
+			                        ),
+		                        ),
+	                        );
+	                        $loop = new WP_Query( $mypost ); ?>
+	                        <?php if ($loop->have_posts()) ?>
+	                        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
                             <div class="swiper-slide">
-<!--                                TODO ACF img-->
-                                <img class="partners-logo" src="<?php echo get_template_directory_uri() . '/assets/src/img/components/our-partners/logo.png' ?>" alt="">
+                                <?php
+
+                                $image_id = get_post_thumbnail_id();
+
+                                $image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+
+                                $image_title = get_the_title( $image_id );
+
+                                ?>
+                                <img class="partners-logo" src="<?php echo get_the_post_thumbnail_url() ?>" alt="<?php echo $image_title ?>">
                             </div>
-                            <div class="swiper-slide">
-                                <!--                                TODO ACF img-->
-                                <img class="partners-logo" src="<?php echo get_template_directory_uri() . '/assets/src/img/components/our-partners/logo.png' ?>" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <!--                                TODO ACF img-->
-                                <img class="partners-logo" src="<?php echo get_template_directory_uri() . '/assets/src/img/components/our-partners/logo.png' ?>" alt="">
-                            </div>
+		                        <?php wp_reset_postdata(); ?>
+	                        <?php endwhile; ?>
                         </div>
                     </div>
                 </div>
