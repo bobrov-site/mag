b8532cd64e2f31e443a9b3955fd1cd2b432261fd
Daniil Bobrov <bobdaniil@yandex.ru>
2022-02-28 15:30:40 +0300

    removed comments from site

--- configure/configure.php
+++ configure/configure.php
@@ -207,3 +207,27 @@ class bootstrap_5_wp_nav_menu_walker extends Walker_Nav_menu {
 
 // register a new menu
 register_nav_menu( 'main-menu', 'Main menu' );
+
+
+// Removes from admin menu
+add_action( 'admin_menu', 'my_remove_admin_menus' );
+function my_remove_admin_menus() {
+	remove_menu_page( 'edit-comments.php' );
+}
+
+// Removes from post and pages
+add_action( 'init', 'remove_comment_support', 100 );
+
+function remove_comment_support() {
+	remove_post_type_support( 'post', 'comments' );
+	remove_post_type_support( 'page', 'comments' );
+}
+
+// Removes from admin bar
+function mytheme_admin_bar_render() {
+	global $wp_admin_bar;
+	$wp_admin_bar->remove_menu( 'comments' );
+}
+
+add_action( 'wp_before_admin_bar_render', 'mytheme_admin_bar_render' );
+
