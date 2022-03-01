2313ecd38571b899cbafe2e92144258af80492ce
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-01 10:47:15 +0300

    created certificates.php loop

--- templates/pages/company/components/certificates.php
+++ templates/pages/company/components/certificates.php
@@ -12,22 +12,26 @@
             <div class="col-xl-12">
                 <div class="certificates-swiper">
                     <div class="swiper-wrapper">
-                        <!--                            TODO ACF-->
+	                    <?php
+	                    $mypost = array(
+		                    'post_type' => 'certificates',
+		                    'posts_per_page' => '9'
+	                    );
+	                    $loop = new WP_Query( $mypost ); ?>
+	                    <?php if ($loop->have_posts()) ?>
+	                    <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
                         <div class="swiper-slide">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/pages/about/certification.png' ?>" alt="">
+                            <?php
+	                        $image_id = get_post_thumbnail_id();
+
+	                        $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+
+	                        $image_title = get_the_title($image_id);
+                            ?>
+                            <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="<?php echo $image_title ?>">
                         </div>
-                        <div class="swiper-slide">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/pages/about/certification2.png' ?>" alt="">
-                        </div>
-                        <div class="swiper-slide">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/pages/about/certification.png' ?>" alt="">
-                        </div>
-                        <div class="swiper-slide">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/pages/about/certification2.png' ?>" alt="">
-                        </div>
-                        <div class="swiper-slide">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/dist/img/pages/about/certification2.png' ?>" alt="">
-                        </div>
+		                    <?php wp_reset_postdata(); ?>
+	                    <?php endwhile; ?>
                     </div>
                     <div class="swiper-controls w-100 d-flex justify-content-center">
                         <div class="swiper-button-prev swipers"></div>
