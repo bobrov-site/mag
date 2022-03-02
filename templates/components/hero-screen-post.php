3c276d23384228125e859e0c1f3a811374d1a582
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-02 14:53:30 +0300

    created hero-screen-post.php for custom thumb

---
+++ templates/components/hero-screen-post.php
@@ -0,0 +1,4 @@
+<section class="hero-screen" style="background: linear-gradient(0deg,rgba(0,0,0,.4),rgba(0,0,0,.4)) ,url(<?php echo get_the_post_thumbnail_url()?>); background-size: cover;">
+	<?php get_template_part('templates/components/header-transparent'); ?>
+	<?php get_template_part('templates/components/breadcrumbs') ?>
+</section>
