f07c6b800ff0b9c4a801826b68cf28d13641a1c2
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-02 14:49:07 +0300

    add acf to hero screen

--- templates/components/hero-screen.php
+++ templates/components/hero-screen.php
@@ -1,5 +1,4 @@
-<!--            //TODO ACF -->
-<section class="hero-screen" style="background: linear-gradient(0deg,rgba(0,0,0,.4),rgba(0,0,0,.4)) ,url(<?php echo get_template_directory_uri() . '/assets/dist/img/pages/main-page/hero-screen/hero-bg.jpg'?>) center center; background-size: cover;">
+<section class="hero-screen" style="background: linear-gradient(0deg,rgba(0,0,0,.4),rgba(0,0,0,.4)) ,url(<?php the_field('img_bg');?>) center center; background-size: cover;">
 	<?php get_template_part('templates/components/header-transparent'); ?>
 	<?php get_template_part('templates/components/breadcrumbs') ?>
 </section>
