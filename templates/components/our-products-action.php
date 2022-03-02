8bd15973eebb4010c2aab78c7ee1f5028c51d60f
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-02 11:24:21 +0300

    add acf to our-products-action.php

--- templates/components/our-products-action.php
+++ templates/components/our-products-action.php
@@ -1,8 +1,24 @@
+<?php
+$mypost = array(
+	'post_type' => 'sections',
+	'posts_per_page' => '1',
+	'title' => 'Наша продукция'
+);
+$loop = new WP_Query( $mypost ); ?>
+<?php if ($loop->have_posts()) ?>
+<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+<?php
+$image_id = get_post_thumbnail_id();
+
+$image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+
+$image_title = get_the_title($image_id);
+?>
 <section class="our-products-action">
 	<div class="container">
 		<div class="row">
 			<div class="col-xl-7">
-				<img class="our-products-action-img" src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/our-products/product.png' ?>" alt="our-products-action">
+				<img class="our-products-action-img" src="<?php echo get_the_post_thumbnail_url() ?>" alt="<?php echo $image_title ?>">
 			</div>
 			<div class="col-xl-5 d-flex align-items-center">
 				<div class="our-products-action-offer">
@@ -29,3 +45,5 @@
 	</div>
     <img class="gradient-section" alt="gradient-products" src="<?php echo get_template_directory_uri() . '/assets/dist/svg/components/our-products/gradient.svg'?>">
 </section>
+	<?php wp_reset_postdata(); ?>
+<?php endwhile; ?>
