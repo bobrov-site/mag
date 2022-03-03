ebd49caf628095187b23258f3da6d9fb3e73507c
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-03 11:51:07 +0300

    header-white.php

---
+++ templates/components/breadcrumbs-dark.php
@@ -0,0 +1,27 @@
+<div class="container">
+	<div class="row">
+		<div class="col-xl-12">
+			<div class="breadcrumbs dark">
+				<?php
+				if (function_exists('yoast_breadcrumb') ) {
+					yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+				}
+				?>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xl-12">
+			<h1 class="title-page dark">
+				<?php
+				if (is_archive()) {
+					single_term_title();
+				}
+				else {
+					the_title();
+				}
+				?>
+			</h1>
+		</div>
+	</div>
+</div>
