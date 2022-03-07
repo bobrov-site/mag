a69401312d477d847595a5923761e90d5d8a9438
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-07 16:52:36 +0300

    created carbon fields for products

--- functions.php
+++ functions.php
@@ -45,6 +45,11 @@ function crb_attach_theme_options() {
 		'plural_name' => 'Характеристики',
 		'singular_name' => 'Характеристику',
 	);
+	$benefit_labels = array(
+		'plural_name' => 'Преимущества',
+		'singular_name' => 'Преимущество'
+	);
+
 	Container::make( 'post_meta', 'Товар' )
 	         ->where( 'post_type', '=', 'product' )
 		->add_tab( __( 'Фотографии' ), array(
@@ -75,6 +80,46 @@ function crb_attach_theme_options() {
 			))
 			->set_max(4)
 		) );
+
+	Container::make( 'post_meta', 'Производство' )
+	         ->where( 'post_type', '=', 'production' )
+	         ->add_tab( __( 'Фотографии' ), array(
+		         Field::make('complex', 'crb_slider', __('Слайдер') )
+		              ->setup_labels( $slider_labels )
+		              ->add_fields(array(
+			              Field::make( 'image', 'crb_image', __( 'Изображение' ) ),
+		              ))
+	         ) )
+	         ->add_tab( __( 'Характеристики' ), array(
+		         Field::make('complex', 'crb_characteristics', __('Характеристика') )
+		              ->setup_labels( $char_labels )
+		              ->add_fields(array(
+			              Field::make( 'text', 'crb_name', __( 'Название Характеристики' ) ),
+			              Field::make( 'text', 'crb_value', __( 'Значение Характеристики' ) ),
+		              ))
+	         ) )
+	         ->add_tab( __( 'Дополнительная информация' ), array(
+		         Field::make('file', 'crb_file', __('Коммерческое предложение'))
+	         ) )
+	         ->add_tab( __( 'С этим товаром покупают' ), array(
+		         Field::make('association', 'crb_association', __('Выбрать товары'))
+		              ->set_types(array(
+			              array(
+				              'post_type' => 'product',
+				              'type' => 'post'
+			              )
+		              ))
+		              ->set_max(4)
+	         ) )
+			->add_tab(__('Преимущества'), array(
+				Field::make('complex', 'crb_benefits', __('Список Преимуществ') )
+				->setup_labels($benefit_labels)
+				->add_fields(array(
+					Field::make('text', 'crb_title', __('Заголовок преимущества') ),
+					Field::make('text', 'crb_desc' , __('Описание ')),
+					Field::make('image', 'crb_image', __('Изображение преимущества'))
+				))
+			));
 
 	Container::make('term_meta', 'Запчасти')
 		->add_fields(array(
