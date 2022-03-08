8f45d5afc4099f33fc8a7dd5127c2b0a0aca1bb0
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-08 17:29:19 +0300

    created details for single product

---
+++ templates/pages/product/details.php
@@ -0,0 +1,20 @@
+<section class="details">
+
+    <div class="details-img">
+        <img src="<?php echo get_template_directory_uri() . '/assets/src/img/pages/product/details.png' ?>" alt="">
+        <div class="details-point" style="top:<?php ?>; right: <?php ?>; bottom: <?php ?>; left: <?php ?>;">
+            <a class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"></a>
+            <ul class="dropdown-menu">
+                <li>
+                    <h3>title</h3>
+                </li>
+                <li>
+                    <img src="<?php echo get_template_directory_uri() . '/assets/src/img/pages/product/point-img.png' ?>" alt="">
+                </li>
+                <li>
+                    <p>desc</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
