252c42aea5f06e218b6284641a2daeacd17797f0
TheWalkingDan14 <39225852+TheWalkingDan14@users.noreply.github.com>
2022-03-08 23:32:29 +0300

    Revert "Production"

--- functions.php
+++ functions.php
@@ -45,24 +45,8 @@ function crb_attach_theme_options() {
 		'plural_name' => 'Характеристики',
 		'singular_name' => 'Характеристику',
 	);
-	$benefit_labels = array(
-		'plural_name' => 'Преимущества',
-		'singular_name' => 'Преимущество'
-	);
-	$details_labels = array(
-		'plural_name' => 'Точка',
-		'singular_name' => 'Точки'
-	);
-
 	Container::make( 'post_meta', 'Товар' )
 	         ->where( 'post_type', '=', 'product' )
-		->add_tab(__('Производитель'), array(
-			Field::make('select', 'crb_developer', __('Кто производитель товара?'))
-			->add_options(array(
-				'MAG' => __('МАГ'),
-				'DEV' => __('Сторонний производитель'),
-			))
-		))
 		->add_tab( __( 'Фотографии' ), array(
 			Field::make('complex', 'crb_slider', __('Слайдер') )
 				->setup_labels( $slider_labels )
@@ -90,45 +74,7 @@ function crb_attach_theme_options() {
 				)
 			))
 			->set_max(4)
-		) )
-		->add_tab(__('Обьект детально'), array(
-			Field::make( 'separator', 'crb_separator_top', __( 'Главное изображение' ) ),
-			Field::make('image', 'crb_bg', __('Изображение')),
-			Field::make('complex', 'crb_points', __('Детальный просмотр обьектов товара') )
-			     ->set_conditional_logic(array(
-				     array(
-					     'field' => 'crb_developer',
-					     'value' => 'MAG',
-					     'compare' => '=',
-				     )
-			     ))
-			     ->setup_labels($details_labels)
-			     ->add_fields(array(
-				     Field::make( 'separator', 'crb_separator_middle', __( 'Координаты точки' ) ),
-				     Field::make('text' , 'crb_top', __('Точка top')),
-				     Field::make('text' , 'crb_left', __('Точка left')),
-				     Field::make( 'separator', 'crb_separator_bottom', __( 'Данные для всплывающего окна' ) ),
-				     Field::make('image', 'crb_image', __('Изображение') ),
-				     Field::make('text', 'crb_title', __('Заголовок') ),
-				     Field::make('text', 'crb_desc', __('Описание') ),
-			     )),
-		))
-		->add_tab(__('Преимущества'), array(
-			Field::make('complex', 'crb_benefits', __('Список Преимуществ') )
-				->set_conditional_logic(array(
-					array(
-						'field' => 'crb_developer',
-						'value' => 'MAG',
-						'compare' => '=',
-					)
-				))
-			     ->setup_labels($benefit_labels)
-			     ->add_fields(array(
-				     Field::make('text', 'crb_title', __('Заголовок преимущества') ),
-				     Field::make('textarea', 'crb_desc' , __('Описание ')),
-				     Field::make('image', 'crb_image', __('Изображение преимущества'))
-			     ))
-		));
+		) );
 
 	Container::make('term_meta', 'Запчасти')
 		->add_fields(array(
