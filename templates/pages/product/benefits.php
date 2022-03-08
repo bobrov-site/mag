b5c49c48938a0f51119b21b4118310df834dbba4
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-08 11:40:05 +0300

    benefits created

--- templates/pages/product/benefits.php
+++ templates/pages/product/benefits.php
@@ -1,3 +1,6 @@
+<?php
+$benefits = carbon_get_post_meta(get_the_ID(), 'crb_benefits');
+?>
 <div class="container">
     <div class="row">
         <div class="col-xl-12">
@@ -8,6 +11,7 @@
         </div>
     </div>
 </div>
+<?php foreach ($benefits as $benefit) { ?>
 <section class="benefits-product">
     <div class="container">
         <div class="row">
@@ -17,15 +21,15 @@
                         <div class="col-xl-5 d-flex align-items-center">
                             <div class="benefits-product-offer">
                                 <h2 class="title-section">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    <?php echo $benefit['crb_title'] ?>
                                 </h2>
                                 <p class="desc-section">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada elementum arcu sit scelerisque nunc enim purus. Ut quam urna aliquam ac tortor eget ultricies nulla. Ultrices ac, sit etiam mi, vel aliquet sed phasellus pharetra. Egestas amet, amet, eros, luctus integer.
+                                    <?php echo $benefit['crb_desc'] ?>
                                 </p>
                             </div>
                         </div>
                         <div class="col-xl-7 d-flex">
-                            <img class="benefits-product-img" src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/our-products/product.png' ?>" alt="">
+                            <img class="benefits-product-img" src="<?php echo wp_get_attachment_url($benefit['crb_image'])  ?>" alt="<?php echo get_the_title($benefit['crb_image']) ?>">
                         </div>
                     </div>
                 </div>
@@ -33,28 +37,4 @@
         </div>
     </div>
 </section>
-<section class="benefits-product">
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-12">
-                <div class="benefits-product-box">
-                    <div class="row">
-                        <div class="col-xl-5 d-flex align-items-center">
-                            <div class="benefits-product-offer">
-                                <h2 class="title-section">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                </h2>
-                                <p class="desc-section">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada elementum arcu sit scelerisque nunc enim purus. Ut quam urna aliquam ac tortor eget ultricies nulla. Ultrices ac, sit etiam mi, vel aliquet sed phasellus pharetra. Egestas amet, amet, eros, luctus integer.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-xl-7 d-flex">
-                            <img class="benefits-product-img" src="<?php echo get_template_directory_uri() . '/assets/dist/img/components/our-products/product.png' ?>" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<?php } ?>
