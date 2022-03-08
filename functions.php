b793afe62a056c9d374dfce1d3045786fba13ec8
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-08 22:57:51 +0300

    created loop with details

--- functions.php
+++ functions.php
@@ -92,6 +92,8 @@ function crb_attach_theme_options() {
 			->set_max(4)
 		) )
 		->add_tab(__('Обьект детально'), array(
+			Field::make( 'separator', 'crb_separator_top', __( 'Главное изображение' ) ),
+			Field::make('image', 'crb_bg', __('Изображение')),
 			Field::make('complex', 'crb_points', __('Детальный просмотр обьектов товара') )
 			     ->set_conditional_logic(array(
 				     array(
@@ -102,16 +104,14 @@ function crb_attach_theme_options() {
 			     ))
 			     ->setup_labels($details_labels)
 			     ->add_fields(array(
-				     Field::make( 'separator', 'crb_separator_top', __( 'Главное изображение' ) ),
-					 Field::make('image', 'crb_bg', __('Изображение')),
 				     Field::make( 'separator', 'crb_separator_middle', __( 'Координаты точки' ) ),
 				     Field::make('text' , 'crb_top', __('Точка top')),
 				     Field::make('text' , 'crb_right', __('Точка right')),
 				     Field::make('text' , 'crb_bottom', __('Точка bottom')),
 				     Field::make('text' , 'crb_left', __('Точка left')),
 				     Field::make( 'separator', 'crb_separator_bottom', __( 'Данные для всплывающего окна' ) ),
-				     Field::make('text', 'crb_title', __('Заголовок') ),
 				     Field::make('image', 'crb_image', __('Изображение') ),
+				     Field::make('text', 'crb_title', __('Заголовок') ),
 				     Field::make('text', 'crb_desc', __('Описание') ),
 			     )),
 		))
--- templates/pages/product/details.php
+++ templates/pages/product/details.php
@@ -1,20 +1,29 @@
+<?php
+$details = carbon_get_post_meta(get_the_ID(), 'crb_points');
+$bg = carbon_get_post_meta(get_the_ID(), 'crb_bg');
+?>
 <section class="details">
-
     <div class="details-img">
-        <img src="<?php echo get_template_directory_uri() . '/assets/src/img/pages/product/details.png' ?>" alt="">
-        <div class="details-point" style="top:<?php ?>; right: <?php ?>; bottom: <?php ?>; left: <?php ?>;">
-            <a class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"></a>
-            <ul class="dropdown-menu">
-                <li>
-                    <h3>title</h3>
-                </li>
-                <li>
-                    <img src="<?php echo get_template_directory_uri() . '/assets/src/img/pages/product/point-img.png' ?>" alt="">
-                </li>
-                <li>
-                    <p>desc</p>
-                </li>
-            </ul>
+        <img src="<?php echo wp_get_attachment_url($bg) ?>" alt="<?php echo get_the_title($bg)?>">
+        <?php foreach ($details as $detail) { ?>
+        <div class="details-point-wrapper">
+            <button class="details-point" style="top:<?php echo $detail['crb_top'] ?>; right: <?php echo $detail['crb_right'] ?>; bottom: <?php echo $detail['crb_bottom'] ?>; left: <?php echo $detail['crb_left'] ?>;" data-bs-toggle="modal" data-bs-target="#exampleModal"></button>
         </div>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <img src="<?php echo wp_get_attachment_url($detail['crb_image'])  ?>" alt="<?php echo get_the_title($detail['crb_image']) ?>">
+                            <h3><?php echo $detail['crb_title'] ?></h3>
+                            <p><?php echo $detail['crb_desc'] ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php }?>
     </div>
 </section>
