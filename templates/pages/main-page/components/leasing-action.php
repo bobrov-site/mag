1ca346084d80343792c36ec5a7f16342590e1306
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-15 19:30:28 +0300

    leasing-action.php 1199px

--- templates/pages/main-page/components/leasing-action.php
+++ templates/pages/main-page/components/leasing-action.php
@@ -1,10 +1,10 @@
 <section class="leasing-action">
 	<div class="container">
 		<div class="row">
-			<div class="col-xl d-flex justify-content-center">
+			<div class="col-md col-xl d-flex justify-content-center">
 				<div style="background: url('<?php echo get_template_directory_uri() . '/assets/dist/img/components/leasing-action/documents.png' ?>') no-repeat" class="leasing-action-img"></div>
 			</div>
-			<div class="col-xl-6 d-flex align-items-center">
+			<div class="col-md col-xl-6 d-flex align-items-center">
 				<div class="leasing-action-offer">
 					<h2 class="title-section dark-section">
 						Важная информация о скидках и лизинговых программах
