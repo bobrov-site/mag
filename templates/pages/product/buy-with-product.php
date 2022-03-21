e802cdd6bf60d20701f37c192b536d163afb6390
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-21 12:02:49 +0300

    deleted animation and added $developer

--- templates/pages/product/buy-with-product.php
+++ templates/pages/product/buy-with-product.php
@@ -15,15 +15,16 @@ $products = carbon_get_post_meta(get_the_ID(), 'crb_association')
 		</div>
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 gy-4 g-sm-4">
 			<?php foreach ($products as $product) { ?>
+                    <?php $developer = carbon_get_post_meta($product['id'], 'crb_developer'); ?>
                 <div class="col">
-                    <div class="card h-100 wow fadeIn">
+                    <div class="card h-100 <?php if ($developer == 'MAG'){ echo 'card-our-product'; } ?>">
                         <img src="<?php echo get_the_post_thumbnail_url($product['id']) ?>" class="card-img-top" alt="">
                         <div class="card-body">
                             <h3 class="card-title">
                                 <?php echo get_the_title($product['id']) ?>
                             </h3>
                             <p class="card-text"><?php echo get_the_excerpt($product['id']) ?></p>
-                            <a class="btn btn-primary" href="<?php echo get_the_permalink($product['id']) ?>">
+                            <a class="btn <?php if ($developer == 'MAG'){ echo 'btn-secondary'; } else { echo 'btn-primary'; }; ?>" href="<?php echo get_the_permalink($product['id']) ?>">
                                 Подробнее
                             </a>
                         </div>
