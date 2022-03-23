304f46880c6819b08289a3f75036403abef0535b
Daniil Bobrov <bobdaniil@yandex.ru>
2022-03-23 13:04:56 +0300

    slider fixed

--- templates/pages/main-page/components/offer.php
+++ templates/pages/main-page/components/offer.php
@@ -16,34 +16,34 @@ $slider = carbon_get_post_meta(get_the_ID(), 'crb_slider');
             <div class="col-xl-12">
                 <div class="swiper-hero">
                     <div class="swiper-wrapper">
-                        <?php foreach ($slider as $slide) {
-                            $product_id = $slide['crb_association'][0]['id']
-                        ?>
-                        <div class="swiper-slide">
-                            <div class="row flex-column-reverse flex-xl-row">
-                                <div class="col-xl-5 d-flex align-items-center">
-                                    <div class="offer">
-                                        <h1 class="offer-title wow fadeInLeft">
-                                            <?php echo $slide['crb_title']?>
-                                        </h1>
-                                        <p class="offer-description wow fadeInLeft">
-                                            <?php echo $slide['crb_desc']?>
-                                        </p>
-                                        <a class="btn-lg offer-link btn-primary wow fadeInLeft" href="<?php echo get_permalink($product_id)?>">
-                                            <?php echo $slide['crb_btn_name'] ?>
-                                        </a>
+			            <?php foreach ($slider as $slide) {
+				            $product_id = $slide['crb_association'][0]['id']
+				            ?>
+                            <div class="swiper-slide">
+                                <div class="row flex-column-reverse flex-xl-row">
+                                    <div class="col-xl-5 d-flex align-items-center">
+                                        <div class="offer">
+                                            <h1 class="offer-title wow fadeInLeft">
+									            <?php echo $slide['crb_title']?>
+                                            </h1>
+                                            <p class="offer-description wow fadeInLeft">
+									            <?php echo $slide['crb_desc']?>
+                                            </p>
+                                            <a class="btn-lg offer-link btn-primary wow fadeInLeft" href="<?php echo get_permalink($product_id)?>">
+									            <?php echo $slide['crb_btn_name'] ?>
+                                            </a>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-xl-7 d-xl-flex justify-content-xl-end justify-content-xll-end align-items-center">
-                                    <div class="slider-img wow fadeInRight">
-                                        <?php $src = wp_get_attachment_image_src( $slide['crb_image'], 'hd'); $url = $src[0] ?>
-                                        <img class="lazyload" alt="<?php get_post_meta($slide['crb_image'], '_wp_attachment_image_alt', TRUE); ?>" data-src="<?php echo $url?>">
-                                        <div class="slider-bg"></div>
+                                    <div class="col-xl-7 d-xl-flex justify-content-xl-end justify-content-xll-end align-items-center">
+                                        <div class="slider-img wow fadeInRight">
+								            <?php $src = wp_get_attachment_image_src( $slide['crb_image'], 'hd'); $url = $src[0] ?>
+                                            <img class="lazyload" alt="<?php get_post_meta($slide['crb_image'], '_wp_attachment_image_alt', TRUE); ?>" data-src="<?php echo $url?>">
+                                            <div class="slider-bg"></div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <?php } ?>
+			            <?php } ?>
                     </div>
                     <div class="swiper-pagination"></div>
                 </div>
